f1c81f11feb80a69182f8a26c11b45179d453e51
Matheus <matheusvenanciodc@gmail.com>
2024-10-11 11:03:12 -0300

    foi rapaz

--- flapmoney/MainPage.xaml.cs
+++ flapmoney/MainPage.xaml.cs
@@ -1,11 +1,72 @@
-﻿namespace flapmoney;
-
-public partial class MainPage : ContentPage
+﻿namespace flapmoney
 {
-	public MainPage()
+	public partial class MainPage : ContentPage
 	{
-		InitializeComponent();
+		const int gravidade = 10;
+		const int fps = 300;
+		bool faliceu = true;
+		double LarguraJanela = 0;
+		double AlturaJanela = 0;
+		int velocidade = 80;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		
+
+		void OnGameOverClicked(object s, TappedEventArgs a)
+
+		{
+			FrameGameOver.IsVisible=false;
+			Inicializar();
+			Desenha();
+		}
+
+		void Inicializar()
+		{
+			faliceu=false;
+			Mario.TranslationY=0;
+		}
+		
+		protected override void OnSizeAllocated(double AJ, double LJ)
+		{
+			base.OnSizeAllocated(AJ, LJ);
+			LarguraJanela = LJ;
+			AlturaJanela = AJ;
+		}
+
+		void AndarCanos()
+		{
+			CanoCima.TranslationX -= velocidade;
+			CanoBaixo.TranslationX -= velocidade;
+
+			if (CanoCima.TranslationX < -LarguraJanela)
+			{
+				CanoBaixo.TranslationX = 0;
+				CanoCima.TranslationX = 0;
+			}
+		}
+
+
+		async Task Desenha()
+		{
+			while (!faliceu)
+			{
+				CriaGravidade();
+				AndarCanos();
+				await Task.Delay(fps);
+			}
+		}
+
+		async void CriaGravidade()
+		{
+			Mario.TranslationY += gravidade;
+			
+			
+		}
+
+		
 	}
-
 }
-
